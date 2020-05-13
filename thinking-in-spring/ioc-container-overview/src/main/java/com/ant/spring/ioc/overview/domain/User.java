b09971f1b83507b203376fde2b417f8abf6a723e
= <gaoxx@heytea.com>
2020-05-13 15:27:28 +0800

    最小配置修改

---
+++ thinking-in-spring/ioc-container-overview/src/main/java/com/ant/spring/ioc/overview/domain/User.java
@@ -0,0 +1,98 @@
+package com.ant.spring.ioc.overview.domain;
+
+import com.ant.spring.ioc.overview.enums.City;
+import org.springframework.core.io.Resource;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+/**
+ * <p>
+ * 用户类
+ * </p>
+ *
+ * @author Ant gxstax@163.com
+ * @since 2020-01-03 09:24
+ */
+public class User {
+
+    private Long id;
+
+    private String name;
+
+    private City city;
+
+    private City[] workCities;
+
+    private List<City> lifeCities;
+
+    private Resource configFileLocation;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public City getCity() {
+        return city;
+    }
+
+    public void setCity(City city) {
+        this.city = city;
+    }
+
+    public Resource getConfigFileLocation() {
+        return configFileLocation;
+    }
+
+    public void setConfigFileLocation(Resource configFileLocation) {
+        this.configFileLocation = configFileLocation;
+    }
+
+    public City[] getWorkCities() {
+        return workCities;
+    }
+
+    public void setWorkCities(City[] workCities) {
+        this.workCities = workCities;
+    }
+
+    public List<City> getLifeCities() {
+        return lifeCities;
+    }
+
+    public void setLifeCities(List<City> lifeCities) {
+        this.lifeCities = lifeCities;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", city=" + city +
+                ", workCities=" + Arrays.toString(workCities) +
+                ", lifeCities=" + lifeCities +
+                ", configFileLocation=" + configFileLocation +
+                '}';
+    }
+
+    public static User createUser() {
+        User user = new User();
+        user.setId(1L);
+        user.setName("马以");
+        return user;
+    }
+}
---
+++ thinking-in-spring/ioc-container-overview/src/main/java/com/ant/spring/ioc/overview/enums/City.java
@@ -0,0 +1,15 @@
+package com.ant.spring.ioc.overview.enums;
+
+/**
+ * <p>
+ * 城市枚举
+ * </p>
+ *
+ * @author Ant gxstax@163.com
+ * @since 2020/5/8 11:38 下午
+ */
+public enum City {
+    BEIJING,
+    HANGZHOU,
+    SHANGHAI
+}
